56000312455dab2d9bbf77b9c570808fc01c3b29
lynns1009 <32124554+lynns1009@users.noreply.github.com>
2018-10-08 18:00:26 +1000

    Update siamese.py

--- siamese.py
+++ siamese.py
@@ -1,11 +1,8 @@
 ﻿'''
 Scaffholding code to get you started for the 2nd assignment.
-
-Team 10:
     
     Liying Shi N10020969
-    Wai Wing Chan N9781463
-    Zhiyi Wu N9589147
+  
 
 '''
 import random
